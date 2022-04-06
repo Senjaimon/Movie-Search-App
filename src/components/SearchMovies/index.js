6ceb28af6a7d8381183e7c4a46f87911fbe95372
Zer0 <samratduttaroy008@gmail.com>
2022-04-06 18:37:59 +0530

    Error Handling Done

--- src/components/SearchMovies/index.js
+++ src/components/SearchMovies/index.js
@@ -7,7 +7,9 @@ export default function SearchMovies(){
     const [response,setResponse]=React.useState(true);
     function getMovie(e){
         e.preventDefault();
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6690ea3f7711f134017e08086400f699&language=en-US&query=${query}&page=1&include_adult=false`).then(res=> res.json()).then(data=>{
+        try{
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6690ea3f7711f134017e08086400f699&language=en-US&query=${query}&page=1&include_adult=false`).then(res=> res.json())
+        .then(data=>{
             if(data.results.length){
                 setResponse(true);
                 setMovies(data.results);
@@ -15,7 +17,11 @@ export default function SearchMovies(){
             else{
                 setResponse(false);
             }
-        });
+        }).catch(()=>alert("No internet Connection"));
+    }
+    catch(error){
+        console.log(error);
+    }
     }
     function searchType(e){
         const {value}=e.target;
