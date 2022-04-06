4d6b84003a572875269e0a8f37c56dedc3070be1
Zer0 <samratduttaroy008@gmail.com>
2022-04-06 16:49:04 +0530

    Changes Regarding Validation And Search Made

--- src/components/SearchMovies/index.js
+++ src/components/SearchMovies/index.js
@@ -4,9 +4,18 @@ import Card from '../Card/index'
 export default function SearchMovies(){
     const [query,setQuery]=React.useState('');
     const [movies,setMovies]=React.useState([]);
+    const [response,setResponse]=React.useState(true);
     function getMovie(e){
         e.preventDefault();
-        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6690ea3f7711f134017e08086400f699&language=en-US&query=${query}&page=1&include_adult=false`).then(res=>res.json()).then(data=>setMovies(data.results));
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=6690ea3f7711f134017e08086400f699&language=en-US&query=${query}&page=1&include_adult=false`).then(res=> res.json()).then(data=>{
+            if(data.results.length){
+                setResponse(true);
+                setMovies(data.results);
+            }
+            else{
+                setResponse(false);
+            }
+        });
     }
     function searchType(e){
         const {value}=e.target;
@@ -15,18 +24,31 @@ export default function SearchMovies(){
     const movieList=movies.filter(movie=>movie.overview && movie.release_date && movie.poster_path).map(movie => (
         <Card key={movie.id} poster_path={movie.poster_path} title={movie.title} release_date={movie.release_date} vote_average={movie.vote_average} overview={movie.overview}/>
     ))
+
+    const message=(e)=>{
+        if(query===''){
+            e.target.setCustomValidity('Type a movie name to search');
+        }
+        else{
+            e.target.setCustomValidity('');
+        }
+        return true;
+    };
       return(
-        <div>
+      <div>
             <form className={styles.form} onSubmit={getMovie}>
                 <label className={styles.label} htmlFor='query'>
                     Movie Name
                     </label>
-                    <input className={styles.query} type='text' placeholder='i.e., Sherlock Holmes' name='query' value={query} onChange={searchType}/>
+                    <input className={styles.query} type='text' placeholder='i.e., Sherlock Holmes' name='query' value={query} onChange={searchType} required onInvalid={message} onInput={message}/>
                 
                 <button className={styles.button}>Search</button>
             </form>
-            <div className={styles.card_list}>
+            {response && <div className={styles.card_list}>
                     {movieList}
-            </div>
-        </div>)
+            </div>}
+            {!response && <p className={styles.result}>No Such Movie Found !</p>}
+        </div>
+        
+        )
 }
